7f2803ff941be8378b08dfbfa72685a1634c1214
Dusan <dusanvulic11@gmail.com>
2022-08-07 19:19:47 +0200

    sort added to api

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,4 @@
+const { create } = require("domain");
 const Product = require("../models/product");
 
 const getAllProductsStatic = async(req, res) => {
@@ -6,7 +7,7 @@ const getAllProductsStatic = async(req, res) => {
 };
 
 const getAllProducts = async(req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -21,8 +22,19 @@ const getAllProducts = async(req, res) => {
         queryObject.name = { $regex: name, $options: "i" };
     }
 
-    const products = await Product.find(queryObject);
-    console.log(queryObject);
+    //console.log(queryObject);
+
+    // sort
+
+    //
+    let result = Product.find(queryObject);
+    if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        result = result.sort("createdAt");
+    }
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length });
 };
 
