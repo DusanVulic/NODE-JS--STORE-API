d166969bd15adee0a6a0057929d7659f6a33dcbb
Dusan <dusanvulic11@gmail.com>
2022-08-07 19:38:32 +0200

    select added

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const { create } = require("domain");
 const Product = require("../models/product");
 
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({}).sort("-name price");
+    const products = await Product.find({}).select("name price");
     res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async(req, res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, select } = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -26,13 +26,18 @@ const getAllProducts = async(req, res) => {
 
     // sort
 
-    //
     let result = Product.find(queryObject);
     if (sort) {
         const sortList = sort.split(",").join(" ");
         result = result.sort(sortList);
     } else {
         result = result.sort("createdAt");
+    }
+    //
+    //select
+    if (select) {
+        const selectList = select.split(",").join(" ");
+        result = result.select(selectList);
     }
     const products = await result;
     res.status(200).json({ products, nbHits: products.length });
