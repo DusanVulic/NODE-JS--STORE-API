37efb46fa890f63056d57ea5816763cc2153841f
Dusan <dusanvulic11@gmail.com>
2022-08-07 15:38:09 +0200

    connecting to DB

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 console.log("pokrenuto");
 
+///pokretanje funkcije za koriscenje env fajla
+require("dotenv").config();
+//async errors
+
+/// express
 const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("server is listening on a port 3000...");
+const connectDB = require("./db/connect");
+
+//handling errors and not found
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//middleware
+
+app.use(express.json());
+
+//routes
+
+app.get("/", (req, res) => {
+    res.send('<h1> Store Api</h1> <a href="api/vi//products">products </a>');
 });
+
+//products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+//dynamic port
+
+const port = process.env.PORT || 3000;
+
+const start = async() => {
+    try {
+        //connect DB
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`server is listening on a port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
